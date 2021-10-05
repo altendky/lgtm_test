d009285179bee9424a0b978f649b001896959629
Kyle Altendorf <sda@fstab.net>
2021-10-05 10:43:17 -0400

    again and left_bad

--- src/lgtm_test/bad.py
+++ src/lgtm_test/bad.py
@@ -5,8 +5,18 @@ def create_a_not_really_a_secret():
     return uuid.uuid4()  # lgtm [py/not-sensitive-data]
 
 
+def again():
+    return uuid.uuid4()  # lgtm [py/not-sensitive-data]
+
+
+def left_bad():
+    return uuid.uuid4()
+
+
 def main():
     print(create_a_not_really_a_secret())
+    print(again())
+    print(left_bad())
 
 
 main()
